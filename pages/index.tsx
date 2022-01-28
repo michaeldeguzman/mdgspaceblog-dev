67b17f2aff673737a73465297ced209e5948d7e2
MdG <michael.de.guzman@gmail.com>
2022-01-28 14:49:06 +0800

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,7 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
-          ))}
+          {posts.map((post) => <PostCard post={post} key={post.title} />)}
         </div>
       </div>
       <div className='lg:col-span-4 col-span-1'>
@@ -25,9 +23,6 @@ export default function Home() {
             <PostWidget />
             <Categories />
         </div>
-      </div>
-      <div>
-        
       </div>
     </div>
   )
